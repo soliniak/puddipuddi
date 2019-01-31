bb2c9a05e61e230d39f831ab1f52439ed4f943e5
Rafi <soliniakdlagier@gmail.com>
2019-01-31 14:58:43 +0100

    Added change pudding, change music

--- main.js
+++ main.js
@@ -6,11 +6,117 @@ let height = window.innerHeight;
 
 // menu
 
+// pause and mute
+
+// volume control
+
+let vol = true;
+
+const muteIcon = `<i class="fas fa-volume-mute"></i>`;
+const volumeIcon = `<i class="fas fa-volume-up"></i>`;
+
+const volumeControl = document.querySelector(".volume-icon");
+
+volumeControl.addEventListener("click", e => {
+  e.target.innerHTML = "";
+  if (vol) {
+    e.target.innerHTML = muteIcon;
+    player.mute();
+    vol = false;
+  } else {
+    e.target.innerHTML = volumeIcon;
+    player.unMute();
+    vol = true;
+  }
+});
+
+// play control
+
+let play = true;
+
+const playIcon = `<i class="fas fa-play"></i>`;
+const pauseIcon = `<i class="fas fa-pause"></i>`;
+
+const playControl = document.querySelector(".pause-icon");
+
+playControl.addEventListener("click", e => {
+  e.target.innerHTML = "";
+  if (play) {
+    e.target.innerHTML = playIcon;
+    player.pauseVideo();
+    play = false;
+  } else {
+    e.target.innerHTML = pauseIcon;
+    player.playVideo();
+    play = true;
+  }
+});
+
+// playlist links
+
+let playThisOnYT = link => {
+  player.loadVideoById(link);
+};
+
+const playlist = document.querySelector(".playlist");
+playlist.innerHTML = "";
+const playlistLinks = document.querySelectorAll(".playlist--link");
+
+const playlistList = [
+  { WibfH9qW7g4: "SHAHMEN – MARK (EMR3YGUL Remix) (INFINITY BASS)" },
+  { eTNWrqgL028: "New Popular Songs - Chill Out" },
+  { qp5UtJJbgQE: "Motez - The Future (KC Lights Remix)" }
+];
+
+playlistList.forEach(entry => {
+  Object.entries(entry).forEach(([key, value]) => {
+    let li = document.createElement("li");
+    li.classList.add("playlist--item");
+    let link = document.createElement("a");
+    link.href = "#";
+    link.classList.add("playlist--link");
+    link.dataset.link = key;
+    link.innerText = value;
+    link.addEventListener("click", e => {
+      console.log("video ID: " + e.target.dataset.link);
+      playThisOnYT(e.target.dataset.link);
+    });
+    playlist.appendChild(li).appendChild(link);
+    // playlist.innerHTML += `<li class="playlist--item"><a href="#" class="playlist--link" data-link=${key}>${value}</a></li>`;
+  });
+});
+
+// change pudding
+
+const changeCakeFn = img => {
+  pudding.forEach(el => {
+    el.style.backgroundImage = `url(${img})`;
+  });
+};
+const puddingsContainer = document.querySelector(".puddings__container");
+
+const puddings = [
+  "https://s3.amazonaws.com/assets.ordienetworks.com/misc/Baby%20Ninja.gif",
+  "https://us.v-cdn.net/5020794/uploads/editor/18/mpegbv0idlzd.gif",
+  "https://media.giphy.com/media/EIZCMrLTkVJHa/giphy.gif",
+  "http://www.squeeze.sk/images/loading.gif",
+  "https://cdn2.scratch.mit.edu/get_image/user/10982092_90x90.png",
+  "https://media.giphy.com/media/l4pTfx2qLszoacZRS/giphy.gif"
+];
+
+puddings.forEach(puddingGif => {
+  const puddingImg = document.createElement("img");
+  puddingImg.classList.add("puddings-gif");
+  puddingImg.src = puddingGif;
+  puddingImg.addEventListener("click", e => changeCakeFn(e.target.src));
+  puddingsContainer.appendChild(puddingImg);
+});
+
 // refresh btn
 const refresBtn = document.querySelector(".refresh-icon");
 
 refresBtn.addEventListener("click", () => {
-  location.reload(true);
+  // location.reload();
 });
 
 // change pudding
@@ -34,46 +140,43 @@ function ValidURL(str) {
 
 changeCake.addEventListener("click", () => {
   if (flag) {
-    pudding.forEach(el => {
-      el.style.backgroundImage = `url(${input.value})`;
-    });
+    changeCakeFn(input.value);
   } else {
     console.log("incorrect url");
   }
 });
 
 // open modals
-const faceBtn = document.querySelector(".face-icon");
-const faceModal = document.querySelector(".add--face__container");
-const faceForm = document.querySelector(".add--face");
+// const faceBtn = document.querySelector(".face-icon");
+// const faceModal = document.querySelector(".add--face__container");
+// const faceForm = document.querySelector(".add--face");
 
 const puddingBtn = document.querySelector(".pudding-icon");
 const puddingModal = document.querySelector(".add--pudding__container");
 const puddingForm = document.querySelector(".add--pudding");
 
 const playlistBtn = document.querySelector(".playlist-icon");
-const playlist = document.querySelector(".playlist");
+// const playlist = document.querySelector(".playlist");
+const playlistItem = document.querySelectorAll(".playlist--item");
 
-const closeAll = () => {
+playlistBtn.addEventListener("click", e => {
   puddingModal.classList.remove("show");
-  faceModal.classList.remove("show");
-  playlist.classList.remove("show");
-};
-
-playlistBtn.addEventListener("click", () => {
-  puddingModal.classList.remove("show");
-  faceModal.classList.remove("show");
-  playlist.classList.toggle("show");
+  // faceModal.classList.remove("show");
+  playlistItem.forEach(item => {
+    if (!item.contains(e.target)) {
+      playlist.classList.toggle("show");
+    }
+  });
 });
 
-faceBtn.addEventListener("click", () => {
-  puddingModal.classList.remove("show");
-  playlist.classList.remove("show");
-  faceModal.classList.toggle("show");
-});
+// faceBtn.addEventListener("click", () => {
+//   puddingModal.classList.remove("show");
+//   playlist.classList.remove("show");
+//   faceModal.classList.toggle("show");
+// });
 
 puddingBtn.addEventListener("click", () => {
-  faceModal.classList.remove("show");
+  // faceModal.classList.remove("show");
   playlist.classList.remove("show");
   puddingModal.classList.toggle("show");
 });
@@ -82,9 +185,9 @@ container.addEventListener("click", e => {
   if (!playlist.contains(e.target)) {
     playlist.classList.remove("show");
   }
-  if (!faceForm.contains(e.target)) {
-    faceModal.classList.remove("show");
-  }
+  // if (!faceForm.contains(e.target)) {
+  //   faceModal.classList.remove("show");
+  // }
   if (!puddingForm.contains(e.target)) {
     puddingModal.classList.remove("show");
   }
@@ -137,14 +240,22 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 //    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
+  var videoId = "qp5UtJJbgQE";
   player = new YT.Player("player", {
-    playerVars: { autoplay: 1, controls: 1 },
+    playerVars: {
+      autoplay: 1,
+      controls: 0,
+      showinfo: 0,
+      fs: 0,
+      rel: 0,
+      modestbranding: 1
+    },
     height: "180",
     width: "320",
-    videoId: "qp5UtJJbgQE",
+    videoId: videoId,
     events: {
-      onReady: onPlayerReady
-      // onStateChange: onPlayerStateChange
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
     }
   });
 }
@@ -157,16 +268,29 @@ function onPlayerReady(event) {
 // 5. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 //    the player should play for six seconds and then stop.
-// var done = false;
-// function onPlayerStateChange(event) {
-//   if (event.data == YT.PlayerState.PLAYING && !done) {
-//     setTimeout(stopVideo, 6000);
-//     done = true;
-//   }
-// }
-// function stopVideo() {
-//   player.stopVideo();
-// }
+
+let done = false;
+let indexList = 0;
+
+console.log();
+
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.ENDED && !done) {
+    if (indexList >= playlistList.length) {
+      indexList = 0;
+    }
+    player.loadVideoById(Object.keys(playlistList[indexList]).toString());
+    // player.loadVideoById(myFetchedNextVideoId);
+    console.log(
+      "ended",
+      "next: ",
+      Object.keys(playlistList[indexList]).toString()
+    );
+    done = true;
+    indexList++;
+  }
+  done = false;
+}
 
 // max velocity
 const velMax = 5;
